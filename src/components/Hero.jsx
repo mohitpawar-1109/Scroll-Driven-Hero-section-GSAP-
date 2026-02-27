04d98f57393b24c72d85cd1be958bfaba14e4e0d
Mohit Pawar <mohitpawar1109@gmail.com>
2026-02-27 10:55:35 +0530

    formatted doc

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,39 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Card from './Card'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+gsap.registerPlugin(ScrollTrigger)
 
 const Hero = () => {
+    const carRef = useRef(null);
+
+    
+
+
+
     return (
         <div className=' bg-gray-200 h-screen relative '>
-            <Card id="card1"  percent="53%"  color="bg-amber-400" para="Increase in pickup point use" top="20" right="right-[30%]" />
-            <Card id="card2" percent="72%"  color="bg-blue-400" para="Increase in pickup point use" top="20" right="right-[10%]" />
+            <Card
+                id="card1"
+                percent="53%"
+                color="bg-amber-400"
+                para="Increase in pickup point use"
+                top="20" right="right-[30%]"
+            />
+
+            <Card id="card2"
+                percent="72%"
+                color="bg-blue-400"
+                para="Increase in pickup point use"
+                top="20"
+                right="right-[10%]"
+            />
             <div className=' overflow-hidden bg-gray-950 w-full h-48 flex justify-start  items-center gap-5 text-white absolute top-72 '>
-                <img className='absolute  h-full object-contain left-0' src="https://paraschaturvedi.github.io/car-scroll-animation/McLaren%20720S%202022%20top%20view.png" alt="Car.png" />
+                <img ref={carRef} className='absolute  h-full object-contain left-0' src="https://paraschaturvedi.github.io/car-scroll-animation/McLaren%20720S%202022%20top%20view.png" alt="Car.png" />
                 <div className='flex gap-5 ml-16'>
                     <span className='text-8xl font-bold'>W</span>
                     <span className='text-8xl font-bold'>E</span>
-                    <span className='text-8xl font-bold'>L</span> 
+                    <span className='text-8xl font-bold'>L</span>
                     <span className='text-8xl font-bold'>C</span>
                     <span className='text-8xl font-bold'>O</span>
                     <span className='text-8xl font-bold'>M</span>
@@ -27,8 +49,22 @@ const Hero = () => {
                     <span className='text-8xl font-bold'>Z</span>
                 </div>
             </div>
-            <Card id="card3" percent="23%"  color="bg-red-400"  para="Decrease in pickup point use"  right="right-[35%]"  />
-            <Card id="card4" percent="44%"  color="bg-gray-400"  para="Decrease in pickup point use"  right="right-[12.5%]"  />
+            <Card
+                id="card3"
+                percent="23%"
+                color="bg-red-400"
+                para="Decrease in
+                pickup point use"
+                right="right-[35%]"
+            />
+
+            <Card
+                id="card4"
+                percent="44%"
+                color="bg-gray-400"
+                para="Decrease in pickup point use"
+                right="right-[12.5%]"
+            />
         </div>
     )
 }
